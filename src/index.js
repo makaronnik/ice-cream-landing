a74e3788a021bd4b151b7ca8c64d7540525d4453
AMIHbKA <bishop0013forum@gmail.com>
2022-12-25 09:04:09 +0200

    added new gallery-images-size

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
 // import Swiper JS
-import Swiper, { Autoplay, Pagination, EffectFlip } from 'swiper';
+import Swiper, { Autoplay, Pagination, EffectFade } from 'swiper';
  
 const swiper = new Swiper('.swiper', {
       
   // configure Swiper to use modules
-    modules: [Autoplay, Pagination, EffectFlip ],
+    modules: [Autoplay, Pagination, EffectFade ],
   
   // Optional parameters
   direction: 'horizontal',
   loop: true,
   spaceBetween: 100,
+  speed: 2000,
 
-  // Autoplay
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  //   pauseOnMouseEnter: true,
+  //effect
+  effect: 'fade',
+  fadeEffect: {
+  crossFade: true
+  },
 
-  // },
+  //Autoplay
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+
+  },
 
   // If we need pagination
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
 });
 
