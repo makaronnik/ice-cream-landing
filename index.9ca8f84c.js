db051089fcb2fd01f017735a39e59b8043ee44de
makaronnik <makaron4eg@gmail.com>
2022-12-27 17:32:35 +0000

    Deploying to gh-pages from @ makaronnik/ice-cream-landing@758b8df5031fdb7eebd6f8d51e96d37c45cc0980 🚀

---
+++ index.9ca8f84c.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-buy-open]"),closeModalBtn:document.querySelector("[data-modal-buy-close]"),modal:document.querySelector("[data-modal-buy]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.9ca8f84c.js.map
