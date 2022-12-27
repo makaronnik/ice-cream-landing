887dfad617a773b25f925a1147d7447c93bacb8c
TetianaRudenko <fyilibratgif@gmail.com>
2022-12-27 17:11:57 +0200

    додала змінні, внесла змінни  js до html

--- src/modal-buy.js
+++ src/modal-buy.js
@@ -1,13 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-buy-open]"),
+    closeModalBtn: document.querySelector("[data-modal-buy-close]"),
+    modal: document.querySelector("[data-modal-buy]"),
   };
-
+ 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
+ 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
